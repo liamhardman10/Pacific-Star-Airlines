97d75eb1b1c26ddbb0a21e4c90ef081e2d9f9a3c
liamhardman1 <liamhardman.cs@gmail.com>
2025-09-13 17:22:06 -0500

    Add project files.

---
+++ pacific-star-airlines/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import MileageSignUpPage from './pages/MileageSignUpPage';
+function App() {
+    return <RouterProvider router={router} />;
+
+}
+    export default App;
---
+++ pacific-star-airlines/frontend/src/pages/SignupModal.jsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useModal } from '../../modalSupp/Modal';
+import './FormSignup.css';
